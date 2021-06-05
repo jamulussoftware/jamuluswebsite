bab6d5f682fac12075dc4ea80883092d3c12e5fd
ann0see <ann0see@users.noreply.github.com>
2021-06-05 18:37:08 +0000

    deploy: 09f5dcd4cff1325a9c096a746cedd76a4961798c

---
+++ de/po4a-update-templates.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# You need po4a > 0.54, see https://github.com/mquinson/po4a/releases
+# There is no need for system-wide installation of po4a
+# You may set following variables
+# SRC_DIR folder for original English .md files
+# POT_DIR folder where the .pot template files are created/updated
+# PO_DIR directory where .po files are stored
+
+
+####################################
+# INITIALISE VARIABLES
+####################################
+
+# root of the documentation repository
+SRC_DIR="./wiki/en"
+
+# folder where the .pot template files are created/updated
+if [ -z "$POT_DIR" ] ; then
+    POT_DIR="./translator-files/l10n/templates"
+fi
+
+# place where the po files are
+if [ -z "$PO_DIR" ] ; then
+	PO_DIR="./translator-files/l10n/po"
+fi
+
+####################################
+# TEST IF IT CAN WORK
+####################################
+
+# Check if po4a is installed
+if ! [ -x "$(command -v po4a)" ] ; then
+    echo 'Error: please install po4a.' >&2
+    exit 1
+fi
+
+# Check if source document folder exists in the right place
+if [ ! -d "$SRC_DIR" ] ; then
+    echo "Error: please run this script from the root folder"
+    exit 1
+fi
+
+############################################
+# CREATE/UPDATE .pot TEMPLATES and .po files
+############################################
+
+while IFS= read -r -d '' file
+do
+    # Determine target file/folder names
+    basename=$(basename -s .md "$file")
+    dirname=$(dirname "$file")
+    path="${dirname#$SRC_DIR/}"
+
+    if [ "$dirname" = "$SRC_DIR" ]; then
+        potname=$basename.pot
+    else
+        potname=$path/$basename.pot
+        mkdir -p "$POT_DIR/$path"
+    fi
+
+    # Use source .md files in English to update/create .pot templates and .po files
+    po4a-gettextize \
+        --format asciidoc \
+        --master "$file" \
+        --master-charset "UTF-8" \
+        --po "$POT_DIR/$potname"
+
+    for lang in $(ls "$PO_DIR" ); do
+
+        po_file="$PO_DIR/$lang/${potname%.pot}.po"
+
+        # po4a-updatepo will complain if the following is not met
+        sed -i 's/Content-Type: text\/plain; charset=CHARSET/Content-Type: text\/plain; charset=UTF-8/g' "$po_file"
+
+        if ! po4a-updatepo \
+            --format asciidoc \
+            --master "$file" \
+            --master-charset "UTF-8" \
+            --po "$po_file" ; then
+        echo ""
+        echo "Error updating $lang PO file for: $adoc_file"
+
+        fi
+    done
+
+done <   <(find -L "$SRC_DIR" -name "*.md" -print0)
+
+echo ""
+echo "REMOVE TEMPORARY FILES"
+
+for lang in $(ls "$PO_DIR" ); do
+	rm "./translator-files/l10n/po/$lang/"*.po~
+done
---
+++ pt/po4a-create-all-targets.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# You need po4a > 0.54, see https://github.com/mquinson/po4a/releases
+# There is no need for system-wide installation of po4a
+# You may set the following variables:
+# SRC_DIR folder for original source English .md files
+# PO_DIR directory where .po files are stored
+# PUB_DIR directory to publish the localised files in
+# THRESHOLD translation % below which translated .md files are not generated
+
+####################################
+# INITIALISE VARIABLES
+####################################
+
+THRESHOLD="80"
+SRC_DIR="./wiki/en"
+
+# place where the po files are
+if [ -z "$PO_DIR" ] ; then
+	PO_DIR="./translator-files/l10n/po"
+fi
+
+# place where the localised files will be
+if [ -z "$PUB_DIR" ] ; then
+	PUB_DIR="./wiki/"
+fi
+
+
+####################################
+# TEST IF IT CAN WORK
+####################################
+
+# Check if po4a is installed
+if ! [ -x "$(command -v po4a)" ] ; then
+    echo 'Error: please install po4a.' >&2
+    exit 1
+fi
+
+# Check if source document folder exists in the right place
+if [ ! -d "$SRC_DIR" ] ; then
+	echo "Error: please run this script from the root folder"
+	exit 1
+fi
+
+##################################################
+# REMOVE .md FILE FOLDERS BEFORE REGENERATING THEM
+##################################################
+
+while IFS= read -r -d '' dir
+do
+	lang=$(basename -s .md "$dir")
+	echo "delete $lang folder"
+    cd ./wiki/
+	rm -rf "$lang" 
+    cd ../  
+done <   <(find "$PO_DIR" -mindepth 1 -maxdepth 1 -type d -print0)
+
+########################################################
+# FUNCTION TO CREATE .md FILES FROM .po FILES USING PO4A
+########################################################
+
+use_po_module () {
+	lang=$1
+
+    # Determine target file/folder names
+	while IFS= read -r -d '' file
+	do
+		basename="$(basename -s .md "$file")"
+		dirname=$(dirname "$file")
+		path="${dirname#$SRC_DIR/}"
+
+		if [ "$dirname" = "$SRC_DIR" ]; then
+			potname=${basename}
+			localized_file="$PUB_DIR/$lang/$basename.md"
+		else
+			potname=$path/$basename
+			localized_file="$PUB_DIR/$lang/$path/$basename.md"
+		fi
+
+        # Run po4a-translate and create target files
+		po4a-translate \
+			--format asciidoc \
+			--master "$file" \
+			--master-charset "UTF-8" \
+			--po "$PO_DIR/$lang/$potname.po" \
+			--localized "$localized_file" --localized-charset "UTF-8" \
+			--keep "$THRESHOLD"
+	done <   <(find -L "$SRC_DIR" -name "*.md"  -print0)
+
+}
+
+##########################################################
+# LOOK INTO EACH .po FILE DIR AND RUN PO4A ON EACH OF THEM
+##########################################################
+
+while IFS= read -r -d '' dir
+do
+	lang=$(basename -s .md "$dir")
+	echo "$lang"
+	use_po_module "$lang"   
+done <   <(find "$PO_DIR" -mindepth 1 -maxdepth 1 -type d -print0)
+
