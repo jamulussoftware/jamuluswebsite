61e53dc41fde52ee264a41bafe290ccc6cc124da
ignotus <darylo1@hotmail.com>
2022-06-23 12:35:46 +0200

    Update po4a-cache.sh

--- _po4a-tools/po4a-cache.sh
+++ _po4a-tools/po4a-cache.sh
@@ -6,7 +6,7 @@
 if [[ "$CACHE_HIT" == 'true' ]] ; then
     sudo cp --force --recursive ~/po4a/* /
 else
-    sudo apt install -yq gettext libsgmls-perl libyaml-tiny-perl opensp
+    sudo apt install -yq gettext libsgmls-perl libyaml-tiny-perl opensp translate-toolkit
     wget -O po4a.deb https://github.com/jamulussoftware/assets/raw/main/po4a/po4a_0.66.deb
     sudo dpkg -i po4a.deb
 
@@ -16,7 +16,7 @@ fi
 
 mkdir -p ~/po4a
 
-for dep in po4a libcroco3 libosp5 sgml-base gettext libsgmls-perl libyaml-tiny-perl opensp; do
+for dep in po4a libcroco3 libosp5 sgml-base gettext libsgmls-perl libyaml-tiny-perl opensp translate-toolkit; do
     dpkg -L $dep | while IFS= read -r f; do if test -f $f; then echo $f; fi; done | xargs cp --parents --target-directory ~/po4a/
 done
 fi
