7ddf4613a69948267db85e2613e0c026833661e5
Ignotus <darylo1@hotmail.com>
2022-12-26 09:48:44 +0100

    Correct dependency spelling

--- _po4a-tools/po4a-cache.sh
+++ _po4a-tools/po4a-cache.sh
@@ -6,7 +6,7 @@
 if [[ "$CACHE_HIT" == 'true' ]] ; then
     sudo cp --force --recursive ~/po4a/* /
 else
-    sudo apt install -yq gettext libsgmls-perl libyaml-tiny-perl opensp libsyntax-keyword-try-perl libs-parse-keyword-perl
+    sudo apt install -yq gettext libsgmls-perl libyaml-tiny-perl opensp libsyntax-keyword-try-perl libxs-parse-keyword-perl
     wget -O po4a.deb https://github.com/jamulussoftware/assets/raw/main/po4a/po4a_0.68.deb
     sudo dpkg -i po4a.deb
 
@@ -16,7 +16,7 @@ fi
 
 mkdir -p ~/po4a
 
-for dep in po4a libcroco3 libosp5 sgml-base gettext libsgmls-perl libyaml-tiny-perl opensp libsyntax-keyword-try-perl libs-parse-keyword-perl; do
+for dep in po4a libcroco3 libosp5 sgml-base gettext libsgmls-perl libyaml-tiny-perl opensp libsyntax-keyword-try-perl libxs-parse-keyword-perl; do
     dpkg -L $dep | while IFS= read -r f; do if test -f $f; then echo $f; fi; done | xargs cp --parents --target-directory ~/po4a/
 done
 fi
